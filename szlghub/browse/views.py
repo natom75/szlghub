95a320e873c55175fb5e25a3b07888e708eb2bf4
BognarZsombor <52254412+BognarZsombor@users.noreply.github.com>
2021-01-11 10:14:39 +0100

    templates

--- szlghub/browse/views.py
+++ szlghub/browse/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import	HttpResponse
 
 def home(request):
-	return HttpResponse('<h1>Browse</h1>')
+	context = {"title":"Browse"}
+	return render(request, 'browse.html', context)
