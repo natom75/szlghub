a8cf1947100c3f12adb1e65d6a9f04d9cadddfa9
natom75 <nagytomi919@gmail.com>
2021-02-21 17:29:07 +0100

    finally progress

--- szlghub/users/views.py
+++ szlghub/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
 	if request.method == 'POST':
@@ -17,4 +18,12 @@ def register(request):
 
 @login_required
 def profile(request):
-	return render(request, 'user/profile.html')
+	u_form = UserUpdateForm()
+	p_form = ProfileUpdateForm()
+
+	context = {
+		'u_form': u_form,
+		'p_form': p_form
+	}
+
+	return render(request, 'user/profile.html', context)
