fe777452688ac43484cad178c541c241cb157405
natom75 <nagytomi919@gmail.com>
2021-02-22 18:32:07 +0100

    .

--- szlghub/browse/views.py
+++ szlghub/browse/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import	HttpResponse
+from ..users.models import Profile
 
 def home(request):
 	context = {"title":"Browse"}
-	return render(request, 'browse.html', context)
+
+	data = Profiles.objects.all()
+	prof = {
+    "user": data
+	}
+	return render_to_response('browse.html', prof)
