889682e061320f43dd2fc4450b7b10d4e6ee8227
natom75 <nagytomi919@gmail.com>
2021-02-22 17:35:27 +0100

    de sok progresssss

--- szlghub/users/forms.py
+++ szlghub/users/forms.py
@@ -20,4 +20,4 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['image', 'desc']
+        fields = ['image', 'desc', 'class_in_school', 'year_in_school']
--- szlghub/users/models.py
+++ szlghub/users/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	image = models.ImageField(default='default.png', upload_to='profile_pics')
-	desc = models.TextField(default='null')
+	desc = models.TextField(max_length=512, default='null')
+	year_in_school = models.IntegerField(range(8, 12), default='1')
+	class_in_school = models.TextField(max_length=1, default='F')
+	points = models.IntegerField(default='0')
 
 	def __str__(self):
 		return f'{self.user.username} Profile'
